7773d8e0618c01409f089fb07950010b8ddd2137
Mischa Sumrall <1212508+Malfeasant@users.noreply.github.com>
2023-03-22 16:00:19 -0700

    Progress- file dialog & dragon drop

--- src/main/java/us/malfeasant/logalyzer/App.java
+++ src/main/java/us/malfeasant/logalyzer/App.java
@@ -1,21 +1,82 @@
 package us.malfeasant.logalyzer;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.tinylog.Logger;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    private final Scene scene;
+    private Stage stage;
+
+    public App() {
+        MenuItem open = new MenuItem("Open...");
+        Menu file = new Menu("File");
+        file.getItems().addAll(open);
+        MenuBar menuBar = new MenuBar(file);
+        open.setOnAction(e -> fileOpen(e));
+
+        Label label = new Label(System.getProperty("java.version"));
+        BorderPane pane = new BorderPane(label);
+        pane.setOnDragOver(e -> handleDragOver(e));
+        pane.setOnDragDropped(e -> handleDrop(e));
+        pane.setTop(menuBar);
+        scene = new Scene(pane);
+    }
+
+    private void handleDragOver(DragEvent event) {
+        var db = event.getDragboard();
+        if (db.hasFiles() || db.hasString()) {
+            event.acceptTransferModes(TransferMode.ANY);
+            event.consume();
+        }
+    }
+
+    private void fileOpen(ActionEvent event) {
+        var chooser = new FileChooser();
+        chooser.setTitle("Open S4 Log File(s)");
+        var files = chooser.showOpenMultipleDialog(stage);
+
+        if (!files.isEmpty()) {
+            open(files);
+        }
+    }
+
+    private void handleDrop(DragEvent event) {
+        var db = event.getDragboard();
+        if (db.hasFiles()) {
+            event.setDropCompleted(true);
+            event.consume();
+            Logger.info("File " + db.getFiles() + " dropped.");
+            open(db.getFiles());
+        } // TODO else if? Can we drop Strings? Folder?
+        else Logger.warn("Can't handle this drop: " + db.getContentTypes());
+    }
+
+    private void open(List<File> files) {
+        // TODO
+    }
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label(System.getProperty("java.version"));
-        BorderPane pane = new BorderPane(label);
-        Scene scene = new Scene(pane);
+        this.stage = stage; // this is needed for dialogs...
         stage.setScene(scene);
         stage.show();
     }
