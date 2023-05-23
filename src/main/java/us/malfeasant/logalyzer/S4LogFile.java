256f2813e0f27bdd5c479bc14e4c5a56e8e9deca
Mischa Sumrall <1212508+Malfeasant@users.noreply.github.com>
2023-05-23 16:03:16 -0700

    Just need something to run the Task...

--- src/main/java/us/malfeasant/logalyzer/S4LogFile.java
+++ src/main/java/us/malfeasant/logalyzer/S4LogFile.java
@@ -7,8 +7,10 @@ import java.io.RandomAccessFile;
 
 import org.tinylog.Logger;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 
 /**
  * Represents a single S4 log file- performs analysis and holds statistics
@@ -28,13 +30,25 @@ public class S4LogFile {
     }
 
     void populateDevices() throws IOException {
-        try (var raf = new RandomAccessFile(file, "r")) {
-            for (var line = raf.readLine(); line != null; line = raf.readLine()) {
-                if (line.contains(", Device - ")) {
-                    devices.add(new CashDevice(this, line));
+        Task<Integer> task = new Task<>() {
+            @Override
+            protected Integer call() throws Exception {
+                int count = 0;
+                try (var raf = new RandomAccessFile(file, "r")) {
+                    for (var line = raf.readLine(); line != null; line = raf.readLine()) {
+                        if (line.contains(", Device - ")) {
+                            var dev = new CashDevice(S4LogFile.this, line);
+                            Platform.runLater(() -> {
+                                devices.add(dev);
+                            });
+                            ++count;
+                        }
+                    }
                 }
+                return count;
             }
-        }
+        };
+        // TODO- an Executor?  a Thread? something needs to run the task...
 
         Logger.debug("Added " + devices.size() + " devices.");
     }
